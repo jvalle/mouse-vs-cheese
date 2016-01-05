4b9c6094e88371d307099b954666db47d91fa63d
jvalle <jcvalle@mail.usf.edu>
2016-01-05 12:04:32 -0800

    scaffold game framework

---
+++ server.js
@@ -0,0 +1,9 @@
+var WebpackDevServer = require("webpack-dev-server");
+var webpack = require("webpack");
+var config = require('./webpack.config.js');
+var server = new WebpackDevServer(webpack(config), {
+	contentBase: "out/"
+
+});
+
+server.listen(8084, "0.0.0.0");
